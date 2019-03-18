06710c8152f5ab9902b55c37de782bcad05d34f5
Layne Moseley <layne@radiant.org>
2019-03-18 09:19:45 -0600

    Fix bash syntax

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ "${CIRCLE_BRANCH}" == "master" ]
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
     ./deploy-dev.sh
 fi
 
-if [ "${CIRCLE_BRANCH}" == "staging" ]
+if [ "${CIRCLE_BRANCH}" == "staging" ]; then
     ./deploy-staging.sh
 fi
 
-if [ "${CIRCLE_BRANCH}" == "production" ]
+if [ "${CIRCLE_BRANCH}" == "production" ]; then
     ./deploy-production.sh
 fi
 
